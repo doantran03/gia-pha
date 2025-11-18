88d652421963d8c008fac6b051af5f4028221c54
doan03 <doan.tran03@py-media.com>
2025-11-18 16:24:18 +0700

    test deloy pc

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       {/* <h1>Vite + React</h1> */}
-      <h1>Test Deloy</h1>
+      <h1>Dev Lo</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
